e571740d32d91d8349ca562ac43ad3fc7bf43906
Irfan Varren <irfanvarren7@gmail.com>
2021-01-27 10:08:10 +0700

    database configuration

--- config/database.php
+++ config/database.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"],1);
 return [
 
     /*
@@ -62,7 +66,16 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
+	'heroku_mysql' => [
+	'driver' => 'mysql',
+	'host' => $host,
+	'database' => $database,
+	'username' => $username,
+	'password' => $password,
+	'charset' => 'utf8',
+	'collation' => 'utf8_unicode_ci',
+	'prefix' => '',
+	],
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
